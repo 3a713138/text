1609a1ca93f930c7af4d6f393dab480ebcb9782e
User <User@LAPTOP-A9QRAGO4>
2020-11-11 20:27:20 +0800

    完成

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnt0 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bnt0
+            // 
+            this.bnt0.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.bnt0.Location = new System.Drawing.Point(555, 32);
+            this.bnt0.Name = "bnt0";
+            this.bnt0.Size = new System.Drawing.Size(171, 102);
+            this.bnt0.TabIndex = 0;
+            this.bnt0.Text = "P1按我 第一張牌";
+            this.bnt0.UseVisualStyleBackColor = true;
+            this.bnt0.Click += new System.EventHandler(this.bnt0_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(802, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 102);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "P1補牌";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(555, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 102);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "P2按我 第一張牌";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(802, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 102);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "P2補牌";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(669, 203);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(188, 75);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "重置";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources._53;
+            this.pictureBox3.Location = new System.Drawing.Point(232, 299);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(156, 204);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApp1.Properties.Resources._53;
+            this.pictureBox4.Location = new System.Drawing.Point(71, 299);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(155, 204);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources._53;
+            this.pictureBox2.Location = new System.Drawing.Point(232, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(156, 204);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources._53;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(155, 204);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 579);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bnt0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnt0;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double x, y, z, a,t,i;
+        List<Image> list = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._1);
+            list.Add(Resources._2);
+            list.Add(Resources._3);
+            list.Add(Resources._4);
+            list.Add(Resources._5);
+            list.Add(Resources._6);
+            list.Add(Resources._7);
+            list.Add(Resources._8);
+            list.Add(Resources._9);
+            list.Add(Resources._10);
+            list.Add(Resources._11);
+            list.Add(Resources._12);
+            list.Add(Resources._13);
+            list.Add(Resources._14);
+            list.Add(Resources._15);
+            list.Add(Resources._16);
+            list.Add(Resources._17);
+            list.Add(Resources._18);
+            list.Add(Resources._19);
+            list.Add(Resources._20);
+            list.Add(Resources._21);
+            list.Add(Resources._22);
+            list.Add(Resources._23);
+            list.Add(Resources._24);
+            list.Add(Resources._25);
+            list.Add(Resources._26);
+            list.Add(Resources._27);
+            list.Add(Resources._28);
+            list.Add(Resources._29);
+            list.Add(Resources._30);
+            list.Add(Resources._31);
+            list.Add(Resources._32);
+            list.Add(Resources._33);
+            list.Add(Resources._34);
+            list.Add(Resources._35);
+            list.Add(Resources._36);
+            list.Add(Resources._37);
+            list.Add(Resources._38);
+            list.Add(Resources._39);
+            list.Add(Resources._40);
+            list.Add(Resources._41);
+            list.Add(Resources._42);
+            list.Add(Resources._43);
+            list.Add(Resources._44);
+            list.Add(Resources._45);
+            list.Add(Resources._46);
+            list.Add(Resources._47);
+            list.Add(Resources._48);
+            list.Add(Resources._49);
+            list.Add(Resources._50);
+            list.Add(Resources._51);
+            list.Add(Resources._52);
+            list.Add(Resources._53);
+        }
+       
+
+        private void bnt0_Click(object sender, EventArgs e)
+        {
+          
+            
+            int index = new Random().Next(51);
+            //模擬隨機產生 一個值;
+            pictureBox1.Image = list[index];
+            x = index;
+            x = (index % 13) + 1;
+            if (x >= 10)
+                x = 0.5;
+        }
+        private void button1_Click(object sender, EventArgs e)
+                {
+                    int index = new Random().Next(51) ;
+                    //模擬隨機產生 一個值;
+                    pictureBox2.Image = list[index];
+                    y = (index%13)+1;
+                    if (y > 10)
+                        y = 0.5;
+                    t = y + x;
+                    if(t>10.5)
+                    MessageBox.Show($"爆了", "Random Num", MessageBoxButtons.OK);
+                    else
+                    MessageBox.Show($"{t}", "Random Num", MessageBoxButtons.OK);
+                }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = list[52];
+            pictureBox2.Image = list[52];
+            pictureBox3.Image = list[52];
+            pictureBox4.Image = list[52];
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = new Random().Next(51);
+            //模擬隨機產生 一個值;
+            pictureBox4.Image = list[index];
+            z = (index % 13) + 1;
+            if (z >= 10)
+                z = 0.5;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int index = new Random().Next(51);
+            //模擬隨機產生 一個值;
+            pictureBox3.Image = list[index];
+            a = (index % 13) + 1;
+            if (a > 10)
+                a = 0.5;
+            i = a + z;
+            if (i > 10.5)
+                MessageBox.Show($"爆了", "Random Num", MessageBoxButtons.OK);
+            else
+                MessageBox.Show($"{i}", "Random Num", MessageBoxButtons.OK);
+        }
+
+    }
+}
